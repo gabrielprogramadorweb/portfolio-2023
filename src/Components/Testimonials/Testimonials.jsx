a29d042529fef5f40111667f8471320d95056cff
gabrielprogramadorads <gabriel.programador.ads@gmail.com>
2023-10-09 22:01:09 -0300

    att

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -35,7 +35,7 @@ const Testimonials = () => {
             <div className="t-heading">
                 <span>Os clientes sempre recebem</span>
                 <span> um trabalho excepcional</span>
-                <span> de mim...</span>
+                <span> de mim.</span>
                 <div className="blur t-blur1" style={{ background: 'var(--purple' }}></div>
                 <div className="blur t-blur2" style={{ background: 'skyblue' }}></div>
             </div>
